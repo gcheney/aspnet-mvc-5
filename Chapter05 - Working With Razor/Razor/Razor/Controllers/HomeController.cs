f1b334666f24c69a403d0dd38f80811cd0396b09
Glendon Cheney <glendon.cheney@gmail.com>
2016-07-06 13:12:31 -0500

    Complete chapter 5

---
+++ Chapter05 - Working With Razor/Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product myProduct = new Product {
+            ProductID = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult NameAndPrice()
+        {
+            return View(myProduct);
+        }
+
+        public ActionResult DemoExpression()
+        {
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(myProduct);
+        }
+
+        public ActionResult DemoArray()
+        {
+            Product[] prodArray = {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "LifeJacket", Price = 48.95M},
+                new Product {Name = "Paddle", Price = 15.5M}
+            };
+
+            return View(prodArray);
+        }
+	}
+}
